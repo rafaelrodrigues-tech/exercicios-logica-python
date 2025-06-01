30887e8aba115466b121166bd4ed596d176d99c9
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-06-01 16:00:22 -0300

    Update ex7.py

--- Variáveis, tipos de dados e atribuição/ex7.py
+++ Variáveis, tipos de dados e atribuição/ex7.py
@@ -1,3 +1,9 @@
 """ Faça um programa que receba o salário-base 
 de  um  funcionário,  calcule  e  mostre  o  seu  salário  a  receber,  sabendo-se  que  esse 
 funcionário teve gratificação de R$ 600,00 e paga imposto de 10% sobre o salário base."""
+
+sal = float(input("Insira seu Salário: "))
+imp = sal - (sal * 10) / 100 # Calculo do imposto.
+new_sal = 600 + imp # Calculo do novo salário
+
+print(f"Novo Salário a receber: \033[32mR${new_sal:.2f}\033[m") # Aumento do salário na cor verde para das destaque.
