a68249707972e9058142079c915750354c4fcdf3
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-07-09 18:57:57 -0300

    Update m00.py

--- Estrutura de Dados/m00.py
+++ Estrutura de Dados/m00.py
@@ -3,15 +3,27 @@ print("Insira valores na matriz a seguir:\n")
 for i in range(3):
     linha = []
     for j in range(4):
-        num = int(input(f"Na linha {i} coluna {j}: "))
-        linha.append(num)
-        m.append(linha)  
+        linha.append(int(input(f"Na linha {i} coluna {j}: ")))
+    m.append(linha)  
     print()    
 
-# Busca por elementos da matriz
+maior_ele = m[0][0]
+menor_ele = m[0][0]
+posicao_maior = (0,0)
+posicao_menor = (0,0)
+
+for i in range(3):
+    for j in range(4):
+        if m[i][j] > maior_ele:
+            maior_ele = m[i][j]
+            posicao_maior = (i,j)
+        if m[i][j] < menor_ele:
+            menor_ele = m[i][j]
+            posicao_menor = (i,j)
+
+print("Matriz Inserida:")
 for linha in m:
-    for num in linha:
-        print(num,end=" ")    
-    print()    
+    print(linha)
 
-    
+print(f"O maior elemento é o {maior_ele} na posição {posicao_maior}.")
+print(f"O menor elemento é o {menor_ele} na posição {posicao_menor}.")    
