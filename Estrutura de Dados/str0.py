78f4446fc2f6e108da3e1b5ccf39d416662d94b5
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-07-09 20:04:44 -0300

    Create str0.py

---
+++ Estrutura de Dados/str0.py
@@ -0,0 +1,13 @@
+nome = str(input("Nome: "))
+sobrenome = str(input("Sobrenome: "))
+
+nome_sobrenome = nome +" "+ sobrenome 
+comp_nome = len(nome)
+comp_sob = len(sobrenome)
+comp_nomesob = len(nome_sobrenome)
+
+print(f"\nSeu nome completo : {nome_sobrenome}")
+print(f"Seu nome completo tem {comp_nomesob} letras.")
+print(f"Seu nome tem {comp_nome} letras.")
+print(f"Seu sobrenome tem {comp_sob} letras.")
+
