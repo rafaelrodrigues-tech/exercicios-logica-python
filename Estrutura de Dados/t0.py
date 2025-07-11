0a5e8a227d766f32e0d7aa5f509222f75f47b2a3
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-07-10 21:37:13 -0300

    Create t0.py

---
+++ Estrutura de Dados/t0.py
@@ -0,0 +1,9 @@
+tupla = (0,1,3,3,5,7,7,7)
+qtdd = len(tupla)
+
+sete = tupla.count(7)
+
+print(f"Conteúdo: {tupla}")
+print(f"Quantidade de elementos: {qtdd}")
+print(f"São {sete} elementos iguais a 7.")
+print(f"Elemento na quinta posição: {tupla[4]} ")
