ad91fb02a5bb353a83cacf0dda4653d669ffddb1
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-07-12 20:59:54 -0300

    Create c0.py

---
+++ Estrutura de Dados/c0.py
@@ -0,0 +1,16 @@
+A = {1,2,3,4}
+B = {3,4,5,6}
+
+print("~"*45)
+
+print("CONJUNTOS(SETS)".center(45))
+
+print(f"\nPrimeiro conjunto: {A}")
+print(f"Segundo conjunto: {B}")
+
+print(f"\nA União entre A ⋃ B = {A.union(B)}")
+print(f"A intersecção entre A ⋂ B = {A.intersection(B)}")
+print(f"A diferença entre A - B = {A.difference(B)}")
+
+print("~"*45)
+
