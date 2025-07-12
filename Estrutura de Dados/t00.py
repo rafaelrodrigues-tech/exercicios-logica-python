40dd9afeb53f31a5ad4a56a8854e53586eecea0e
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-07-12 20:08:22 -0300

    Create t00.py

---
+++ Estrutura de Dados/t00.py
@@ -0,0 +1,6 @@
+quitanda = ("Maçã", "Laranja", "Banana")
+
+print("Frutas na quitanda: ",end="")
+for fruta in quitanda:
+    print(fruta,end=" ")
+print(f"\nQuantidade de elementos: {len(quitanda)}")    
