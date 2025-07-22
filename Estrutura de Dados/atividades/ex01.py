dbffd85c22e5349da8fb248d264db266f84cfb73
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-07-22 20:08:00 -0300

    Update ex01.py

--- Estrutura de Dados/atividades/ex01.py
+++ Estrutura de Dados/atividades/ex01.py
@@ -2,12 +2,10 @@
 algoritmo deve mostrar o somatório de todos os elementos do vetor, bem como a média 
 aritmética entre todos os termos."""
 
-vetor = []
-print("Preencha o vetor a seguir:")
-for _ in range(6):
-    num = int(input("Número: "))
-    vetor.append(num)
-
-for linha in vetor:
-    for num in linha: 
-        print(num)  
+v = [] # Vetor
+for i in range(10):
+    num = int(input(f"Número para a posição {i+1}: "))
+    v.append(num)
+ 
+print(f"\nSomatório: {sum(v)}") 
+print(f"Média Aritmética: {sum(v) / len(v)}") #len() verifica o tamanho do vetor "v"
