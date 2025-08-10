159a542aa0947c428d1326eebcdb4bff3eae14f6
Rafael Rodrigues <rrobrigues15@gmail.com>
2025-08-10 13:53:23 -0300

    Matriz 3x3 elevado ao quadrado

--- Estrutura de Dados/atividades/ex07.py
+++ Estrutura de Dados/atividades/ex07.py
@@ -1,3 +1,18 @@
 """Desenvolva  um  algoritmo  que  preencha  cada  elemento  de  uma  matriz  3x3  com  o 
 quadrado do valor do índice da linha mais o valor do índice da coluna daquela posição. 
 Ao final, o algoritmo deve mostrar a matriz, na tela."""
+matriz = []
+for i in range(3):
+    linha = []
+    for j in range(3):
+        num = int(input(f"Linha {i} na Coluna {j}: "))
+        linha.append(num)
+    matriz.append(linha)    
+
+for i in range(3):
+    for j in range(3):
+        matriz[i][j] = (i + j) ** 2 # Cálculo
+
+print("\nMatriz elevado ao quadrado: ")
+for linha in matriz:
+    print(linha)
