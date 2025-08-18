238beeb4392c94ba02a599240442418386e46b94
Rafael <rrobrigues15@gmail.com>
2025-08-18 18:58:25 -0300

    Update ex11.py

--- Estrutura de Dados/atividades/ex11.py
+++ Estrutura de Dados/atividades/ex11.py
@@ -1,21 +1,30 @@
-"""Desenvolva um algoritmo que preencha uma matriz numérico de dimensões 3x3. Após 
-preencher todo a matriz, o usuário deve inserir uma chave de busca X. Caso exista algum 
-número igual a X, dentro da matriz, o algoritmo deve mostrar, na tela, os índices da linha 
-e  da  coluna  da  posição  na  qual  na  qual  X  foi  encontrado  pela  primeira  vez.  Caso 
-contrário, o algoritmo deve se encerrar com uma única mensagem, dizendo "Chave não 
-encontrada."."""
-
+"""
+Crie um algoritmo que:
+1. Preencha uma matriz 3x3 com valores inteiros.
+2. Peça ao usuário uma chave de busca X.
+3. Informe a posição (linha, coluna) da primeira ocorrência de X, 
+   ou mostre 'Chave não encontrada.' caso não exista.
+"""
 matriz = []
-for i in range(2):
+for i in range(3):
     linha = []
-    for j in range(2):
+    for j in range(3):
         num = int(input(f"Insira um número na posição ({i},{j}): "))
         linha.append(num)
     matriz.append(linha)
 
-chave_de_busca = int(input("Informe a chave de busca x: "))        
-chave = matriz.index(num)
+chave_de_busca = int(input("\nInforme a chave de busca x: "))    
+achou = False   # Flag para indicar se a chave foi encontrada.
+
+print("\nMatriz preechida:")
 for linha in matriz:
-    for num in linha:
-        if chave_de_busca == num:
-            print(f"Chave encontrada na posição: {chave}")
+    print(" ".join((f"{num:3}")for num in linha)) # centraliza em 3 espaços
+
+for i, linha in enumerate(matriz): #enumerate vai retornar o índice da linha(i).
+    if chave_de_busca in linha:
+        j = linha.index(chave_de_busca) #index, vai procurar pelo primeiro índice da coluna (j).
+        print(f"\nNúmero encontrado na posição ({i},{j})")
+        achou = True 
+        break
+if not achou:
+    print("\nChave não encontrada.")    
